7b825452e77835bcca6f29eab72b8dde8acf762f
Konstantin-vetro <konstantin.vetro@yandex.ru>
2023-03-14 23:20:04 +0500

    Отдельно вынес логику создания алеута в структуру, создал делегат со слабой связью и инъектировал контроллер

---
+++ MovieQuiz/Presentation/AlertPresenter.swift
@@ -0,0 +1,28 @@
+//
+//  AlertPresenter.swift
+//  MovieQuiz
+//
+
+import Foundation
+import UIKit
+//create an alert class
+class AlertPresenter: AlertProtocol {
+    //создаем делегат со слабой ссылкой
+    weak var delegate: AlertDelegate?
+    
+    init(delegate: AlertDelegate) {     //инъекция зависимостей через инициализатор
+        self.delegate = delegate
+    }
+    
+    func createAlertController(from alert: AlertModel) {
+        //создаем alertController
+        let alertController = UIAlertController(title: alert.title, message: alert.message, preferredStyle: .alert)
+        //создаем actionController
+        let alertAction = UIAlertAction(title: alert.buttonText, style: .default, handler: alert.completion)
+        //связываем alert и action
+        alertController.addAction(alertAction)
+        //показ по методу делегата
+        delegate?.presentAlertController(alertController)       
+    }
+
+}
