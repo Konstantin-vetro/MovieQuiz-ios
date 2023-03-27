751dbecc12b18803e2e2a03cc279e895e68ea938
Konstantin-vetro <konstantin.vetro@yandex.ru>
2023-03-28 01:06:48 +0500

    добавил три метода делегата по загрузке данных, ошибочной загрузки и ошибки при загрузки изображения

--- MovieQuiz/Services/QuestionFactoryDelegate.swift
+++ MovieQuiz/Services/QuestionFactoryDelegate.swift
@@ -2,13 +2,14 @@
 //  QuestionFactoryDelegate.swift
 //  MovieQuiz
 //
-//  Created by Гость on 12.03.2023.
-//
+
 
 import Foundation
 
+///Делегаты фабрики вопросов
 protocol QuestionFactoryDelegate: AnyObject {
     func didReceiveNextQuestion(question: QuizQuestion?)    //метод который должен быть у фабрики вопросов
-    func didLoadDataFromServer()    //сообщение об успешной загрузке
-    func didFailToLoadData(with error: Error)   //сообщение об ошибке загрузки
+    func didLoadDataFromServer()                            //сообщение об успешной загрузке
+    func didFailToLoadData(with error: Error)               //сообщение об ошибке загрузки
+    func failedToUploadImage(for quizQuestionIndex: Int)    //метод загрузки изображения при ошибке
 }
