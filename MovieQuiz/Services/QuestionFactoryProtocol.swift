590322da5cf7b8db0c393587bf6396a8fb329c6d
Konstantin-vetro <konstantin.vetro@yandex.ru>
2023-03-28 01:07:42 +0500

    добавил метод перехода на следующий вопрос по индексу в случае ошибки загрузки изображения

--- MovieQuiz/Services/QuestionFactoryProtocol.swift
+++ MovieQuiz/Services/QuestionFactoryProtocol.swift
@@ -1,6 +1,7 @@
 import Foundation
-
+/// Протокол фабрики вопросов
 protocol QuestionFactoryProtocol {
     func requestNextQuestion()
+    func requestNextQuestionByIndex(by index: Int)
     func loadData()
 }
